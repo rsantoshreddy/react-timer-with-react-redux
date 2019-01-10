d9208e9c06bc32e2a4759c0dd3fb171a486ef277
rrsantosh <rrsantosh@juniper.net>
2019-01-11 00:02:35 +0530

    react-timer-with-react-redux

--- src/index.js
+++ src/index.js
@@ -1,39 +1,56 @@
 import React from "react";
 import ReactDOM from "react-dom";
 import { createStore } from "redux";
+import { connect, Provider } from "react-redux";
 
-const view = m => {
-	const { runnig, time } = m;
+const mapStateToProps = state => {
+	return state;
+};
+
+const mapDispatchToProps = dispatch => {
+	return {
+		start: () => {
+			dispatch({ type: "START" });
+		},
+		stop: () => {
+			dispatch({ type: "STOP" });
+		}
+	};
+};
+
+const StopWatch = connect(
+	mapStateToProps,
+	mapDispatchToProps
+)(props => {
+	const { runnig, time, start, stop } = props;
 	const minutes = Math.floor(time / 60);
 	const seconds = time - minutes * 60;
-
-	const handler = () => {
-		container.dispatch(runnig ? { type: "STOP" } : { type: "START" });
-	};
 
 	return (
 		<div>
 			<p>{`${minutes}:${seconds}`}</p>
-			<button onClick={handler}>{runnig ? "STOP" : "START"}</button>
+			<button onClick={runnig ? stop : start}>
+				{runnig ? "STOP" : "START"}
+			</button>
 		</div>
 	);
-};
+});
 
 const update = (model, intent) => {
 	const { time, runnig } = model;
 	switch (intent.type) {
 		case "TICK":
-			model = Object.assign(model, {
+			model = Object.assign({}, model, {
 				time: time + (runnig ? 1 : 0)
 			});
 			break;
 		case "START":
-			model = Object.assign(model, {
+			model = Object.assign({}, model, {
 				runnig: true
 			});
 			break;
 		case "STOP":
-			model = Object.assign(model, {
+			model = Object.assign({}, model, {
 				runnig: false
 			});
 			break;
@@ -44,14 +61,12 @@ const update = (model, intent) => {
 
 const container = createStore(update, { runnig: true, time: 0 });
 
-const render = () => {
-	ReactDOM.render(
-		view(container.getState()),
-		document.getElementById("root")
-	);
-};
-
-container.subscribe(render);
+ReactDOM.render(
+	<Provider store={container}>
+		<StopWatch />
+	</Provider>,
+	document.getElementById("root")
+);
 
 setInterval(() => {
 	container.dispatch({ type: "TICK" });
